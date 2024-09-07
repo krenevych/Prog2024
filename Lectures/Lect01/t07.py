bf78653861926c1642e7cf3e155b5de7dfa02271
Andrii Krenevych <krenevych@knu.ua>
2024-09-07 13:46:52 +0300

    1

---
+++ Lectures/Lect01/t07.py
@@ -0,0 +1,18 @@
+# Ідентифікатори:
+#      літери лат алф.
+#      цифри
+#      _
+
+_my_vatiable_that_I_want_to_use_in_this_program_333333 = 1
+my_vatiable_that_I_want_to_use_in_this_program_333333 = 5
+
+x = 34
+s = 'Hello'
+
+S = "I'm Andrii"
+S2 = 'He sad "Hello"'
+S = 45
+print(S)
+print(S2)
+
+
