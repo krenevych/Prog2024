e79a5888e6542cf5987c90e293727640c806e3b2
Andrii Krenevych <krenevych@knu.ua>
2024-09-16 09:24:27 +0300

    4

---
+++ Lectures/Lect03/t01.py
@@ -0,0 +1,12 @@
+# вивести на екран 5 разів фразу Hello, World!
+
+# n = int(input("Введіть кількість разів скільки вивести на екран фразу Hello, World! "))
+n = 3
+
+i = 1
+while i <= n:
+    print(f"{i}: Hello, World!")
+    i += 1
+pass
+# for i in range(n):
+#     print(f"{i}: Hello, World!")
