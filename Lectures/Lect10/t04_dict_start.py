74b414170370683ab71d31c61ae68d88aa896370
Andrii Krenevych <krenevych@knu.ua>
2024-10-21 10:19:37 +0300

    lect 10

--- Lectures/Lect10/t04_dict_start.py
+++ Lectures/Lect10/t04_dict_start.py
@@ -4,16 +4,16 @@ d = {
     2: "second"
 }
 
-# print(d)
-# print(d["Hello"])
-# d["Hello"] = "hello"
-# print(d)
-# d["python"] = "Best of the best"
-# print(d)
-# del d["python"]
+print(d)
+print(d["Hello"])
+d["Hello"] = "hello"
+print(d)
+d["python"] = "Best of the best"
+print(d)
+del d["python"]
 print(d)
 
-# print(d[123])
+# print(d[123])  # породить помилку, бо елементу з таким ключем немає в словнику
 
 for key in d:
     print(key)
