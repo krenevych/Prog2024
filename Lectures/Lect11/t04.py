09cc09bb8f356c9cede9528658f3154f60d69a07
Andrii Krenevych <krenevych@knu.ua>
2024-10-28 09:32:02 +0200

    lect 11

---
+++ Lectures/Lect11/t04.py
@@ -0,0 +1,10 @@
+d = {'first': 1, 'second': 2}
+
+print(1 in d.values())
+
+my_divine_dictionary = {'first': 1, 'second': 2}
+print(my_divine_dictionary.values())
+
+# elem = my_divine_dictionary.get('second')
+elem = my_divine_dictionary['second']
+print(elem)
---
+++ Lectures/Lect11/t05.py
@@ -0,0 +1,11 @@
+my_divine_dictionary = {'first': 1, 'second': 2}
+
+# elem = my_divine_dictionary.get('second')
+# elem = my_divine_dictionary['third']
+# elem = my_divine_dictionary.get('third', "мене тута немає")
+# print(elem)
+
+# print(my_divine_dictionary.items())
+
+for key, value in my_divine_dictionary.items():
+    print(key, value)
