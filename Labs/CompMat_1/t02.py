6266f97321513cc76f8362909ed4e60bfb798bd2
Andrii Krenevych <krenevych@knu.ua>
2024-11-01 11:34:46 +0200

    lab 9

--- Labs/CompMat_1/t02.py
+++ Labs/CompMat_1/t02.py
@@ -5,5 +5,14 @@ d_freq = {}  # словник де ключ - число, а значення с
 for el in elements:
     d_freq[el] = elements.count(el)
 
-print(d_freq)
-print(max(d_freq.values()))
+# print(d_freq)
+# print(max(d_freq.values()))
+
+N_2 = n // 2
+for el, val in d_freq.items():
+    # print(el, val)
+    if val > N_2:
+        print(el)
+        break
+else:
+    print(-1)
