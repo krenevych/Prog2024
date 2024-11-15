80248cbff0072fb75074db139137777631a206c7
Andrii Krenevych <krenevych@knu.ua>
2024-11-15 11:53:37 +0200

    lab 14

--- Labs/CompMat_1/lab14/t05.py
+++ Labs/CompMat_1/lab14/t05.py
@@ -11,7 +11,14 @@ def readMatrix(filename):
     except FileNotFoundError:
         return None  # якщо файл не існує, то повертаємо None
 
+def writeMatrix(M, filename):
+    with open(filename, 'w') as f:
+        for i in range(len(M)):
+            for j in range(len(M[0])):
+                print(M[i][j], end=" ", file=f)
+            print(file=f)
 
 if __name__ == '__main__':
     M = readMatrix('matrix.txt')
     print(M)
+    writeMatrix(M, 'matrix_out.txt')
