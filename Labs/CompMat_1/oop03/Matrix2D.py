a1c74fbaeefe228cfe06de76ddcce36e7152ae09
Andrii Krenevych <krenevych@knu.ua>
2025-02-28 09:14:54 +0200

    lab oop 04

--- Labs/CompMat_1/oop03/Matrix2D.py
+++ Labs/CompMat_1/oop03/Matrix2D.py
@@ -3,15 +3,26 @@ class Matrix2D:
         if len(a) == 1:  # один параметр, але може в ньому бути або масив або інша матриця
             pass
         elif len(a) == 4: # вхідний параметр - 4 числа, перші два - перший рядок, 3, 4 - другий рядок.
-            pass
+            # self.a11 = a[0]
+            # self.a12 = a[1]
+            # self.a21 = a[2]
+            # self.a22 = a[3]
+
+            self.a11, self.a12, self.a21, self.a22 = a
+
         elif len(a) == 0: # одинична матриця
             pass
         else:
             raise ValueError("По таких даних неможливо створити матрицю!")
 
+    def __str__(self):
+        return f"{self.a11:3.2f} {self.a12:3.2f}\n{self.a21:3.2f} {self.a22:3.2f}"
+
 
 if __name__ == '__main__':
     m = Matrix2D(1, 2, 3, 4)
+    print(m)
+
     # m1 = Matrix2D(m)
     # m2 = Matrix2D(
     #     [
@@ -20,7 +31,7 @@ if __name__ == '__main__':
     #     ]
     # )
     # m3 = Matrix2D()
-    m4 = Matrix2D(1, 2, 3)  #  має бути породжена помилка
+    # m4 = Matrix2D(1, 2, 3)  #  має бути породжена помилка
     #
     # s1 = int("5")
     # s2 = int("5_hello")
