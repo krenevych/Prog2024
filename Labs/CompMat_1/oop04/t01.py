aab0a35719bba7ac8e16cd28c7d2f924bf0e462a
Andrii Krenevych <krenevych@knu.ua>
2025-03-04 09:46:34 +0200

    lect oop 04

--- Labs/CompMat_1/oop04/t01.py
+++ Labs/CompMat_1/oop04/t01.py
@@ -4,27 +4,39 @@ from turtle import * # Підключаємо модуль turtle
 class Triangle:
 
     def __init__(self, x1, y1, x2, y2):
-        self.position = (0, 0)   # абсолютна позиція першої вершини
-        self.vertex1 = (x1, y1)  # позиція другої відносно першої вершини
-        self.vertex2 = (x2, y2)  # позиція третьої відносно першої вершини
+        self.__position = (0, 0)   # абсолютна позиція першої вершини
+        self.__vertex1 = (x1, y1)  # позиція другої відносно першої вершини
+        self.__vertex2 = (x2, y2)  # позиція третьої відносно першої вершини
 
         self.color = "black"     # колір трикутника за промовчанням
 
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, pos):
+        assert isinstance(pos, (tuple, list)) and len(pos) == 2
+        self.__position = pos
+
     def __str__(self):
-        return f"Triangle: {self.position, self.vertex1, self.vertex2}"
+        return f"Triangle: {self.__position, self.__vertex1, self.__vertex2}"
 
     def draw(self):
         up() # перестраховка, якщо пензлик був опущений
 
-        goto(self.position)
+        goto(self.__position)
 
         down()
-        setpos(self.vertex1)
-        setpos(self.vertex2)
-        setpos(self.position)
 
+        # зміщені координати для vertex1
+        x, y = self.__position[0] + self.__vertex1[0], self.__position[1] + self.__vertex1[1]
+        setpos(x, y)
 
-
+        # зміщені координати для vertex1
+        x, y = self.__position[0] + self.__vertex2[0], self.__position[1] + self.__vertex2[1]
+        setpos(x, y)
+        setpos(self.__position)
         up()
 
 
@@ -36,4 +48,13 @@ if __name__ == '__main__':
     speed(1)
     # тут малюємо
     t.draw()
+
+
+    # t.set_position(100, 100)
+    # t.position = (100, 100, 100)
+    # t.position = "green"
+    t.position = (100, 150)
+    t.draw()
+
+
     mainloop()       # Затримуємо вікно на екрані
