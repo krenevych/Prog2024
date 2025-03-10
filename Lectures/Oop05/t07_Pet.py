7cb4999da81a2689c1c01e718f4ea44ac2a404ff
Andrii Krenevych <krenevych@knu.ua>
2025-03-10 10:04:16 +0200

    lect oop 05

---
+++ Lectures/Oop05/t07_Pet.py
@@ -0,0 +1,25 @@
+class Pet:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+class Cat(Pet):
+    def voice(self):
+        print(f"Cat {self._name} says Miu-miu!!!")
+
+class Dog(Pet):
+    def voice(self):
+        print(f"Dog {self._name} says Bau-bau!!!")
+
+if __name__ == '__main__':
+    # p = Pet("Кабан")
+    # p.voice()
+
+    c = Cat("Кабан")
+    c.voice()
+
+    d = Dog("Кулька")
+    d.voice()
