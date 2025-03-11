f7f3732680a00f2395a94b6bb4fafa20ac7169f8
Andrii Krenevych <krenevych@knu.ua>
2025-03-11 21:03:24 +0200

    lect oop 05

---
+++ Lectures/Oop05/t09_Pet.py
@@ -0,0 +1,33 @@
+class Pet:
+    def __init__(self, name):
+        self._name = name
+
+    def voice(self):
+        print(f"I'm base class, I don't know what to say!")
+
+    @property
+    def name(self):
+        return self._name
+
+class Cat(Pet):
+
+    def __init__(self, name, master):
+        super().__init__(name)
+        self.master_name = master
+
+    def __str__(self):
+        return f"{self.name}, {self.master_name}"
+
+    def voice(self):
+        print(f"Cat {self._name} says Miu-miu!!!")
+
+
+
+
+if __name__ == '__main__':
+
+
+    c = Cat("Кабан", "Артур")
+    c.voice()
+    print(c)
+
