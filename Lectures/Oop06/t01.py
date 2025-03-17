d5ec31a06321ad0aa4a15aa2d539666515276d46
Andrii Krenevych <krenevych@knu.ua>
2025-03-17 09:05:47 +0200

    lect06

---
+++ Lectures/Oop06/t01.py
@@ -0,0 +1,24 @@
+class Point2D:
+
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    def abs2(self):
+        return self.x ** 2 + self.y ** 2
+
+    def __str__(self):
+        return f"Point2D: ({self.x}, {self.y})"
+
+
+if __name__ == '__main__':
+    z = Point2D(3, 4)
+    print(z)
