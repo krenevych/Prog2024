f88f6da66f5975b05454308a81c69f58177b8986
Andrii Krenevych <krenevych@knu.ua>
2025-03-25 09:26:54 +0200

    lab 07

---
+++ Labs/CompMat_1/oop07/t01.py
@@ -0,0 +1,48 @@
+class Figure:
+    def perimetr(self): pass
+    def square(self): pass
+    def volume(self) : pass
+    def squareBase(self) : pass
+
+
+class Triangle(Figure):
+    def __init__(self, a, b, c):
+        self.a, self.b, self.c = a, b, c
+
+    def perimetr(self):
+        return self.a + self.b + self.c
+
+    def square(self):
+        p = self.perimetr() / 2  # півпериметр
+        a, b, c = self.a, self.b, self.c
+        s = p * (p - a) * (p - b) * (p - c)
+        return s ** 0.5
+
+    def volume(self) : return None
+    def squareBase(self) : return None
+
+class TrianglePyramid(Triangle):
+    def __init__(self, a, b, c, h):
+        super().__init__(a, b, c) # створюємо трикутник, що є основою піраміди
+        self.h = h
+
+    def perimetr(self):
+        return None
+
+    def square(self):
+        return None
+
+    def volume(self):
+        return 1/3 * self.h * self.squareBase()
+
+    def squareBase(self):
+        return super().square()
+
+
+if __name__ == '__main__':
+    # t = Triangle(3, 4, 5)
+    # print(t.perimetr())
+    # print(t.square())
+
+    tp = TrianglePyramid(3, 4, 5, 3)
+    print(tp.volume())
