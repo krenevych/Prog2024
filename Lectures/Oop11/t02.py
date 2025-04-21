e12af3dc6cf47cc104a3515e14791941ac4ffe9c
Andrii Krenevych <krenevych@knu.ua>
2025-04-21 09:27:48 +0300

    Власні класи виключень

--- Lectures/Oop11/t02.py
+++ Lectures/Oop11/t02.py
@@ -1,8 +1,8 @@
 # Опишемо клас виключення для функції введення з клавіатури
 # лише невід’ємних цілих чисел.
 
-def input_positive_int():
-    s = input()
+def input_positive_int(*args, **kwargs):
+    s = input(*args, **kwargs)
     s = int(s)
     if s < 0:
         raise ValueError
@@ -13,7 +13,7 @@ def input_positive_int():
 if __name__ == '__main__':
     try:
 
-        a = input_positive_int()
+        a = input_positive_int("Задайте ціле невідʼємне значення ")
         print(a, type(a))
     except ValueError:
         print("Введене значення не ціле або відʼємне")
--- Lectures/Oop11/t03.py
+++ Lectures/Oop11/t03.py
@@ -1,19 +1,48 @@
 # Опишемо клас виключення для функції введення з клавіатури
 # лише невід’ємних цілих чисел.
 
-def input_positive_int():
-    s = input()
-    s = int(s)
-    if s < 0:
-        raise ValueError
-    return s
+ERROR_NON_INTEGER = 0
+ERROR_NON_POSITIVE_INTEGER = 1
+
+class InputPositiveIntException(Exception):
+    def __init__(self, message, code_error):
+        super().__init__()
+        self.message = message
+        self.code_error = code_error
+
+    def __str__(self):
+        return str(self.message)
+
+
+def input_positive_int(*args, **kwargs):
+    s = input(*args, **kwargs)
+    try:
+        s = int(s)  # ось тут може бути проблема конвертації до int
+        if s < 0:   # тут може бути проблема, що введене число відʼємне
+            raise InputPositiveIntException(
+                "Введене ціле число не є невідʼємним",
+                ERROR_NON_POSITIVE_INTEGER
+            )
+
+        return s
+
+    except ValueError:
+        raise InputPositiveIntException(
+            "Введене число не цілого типу",
+            ERROR_NON_INTEGER
+        )
+
 
 
 
 if __name__ == '__main__':
     try:
 
-        a = input_positive_int()
+        a = input_positive_int("Задайте ціле невідʼємне значення ")
         print(a, type(a))
-    except ValueError:
-        print("Введене значення не ціле або відʼємне")
+    except InputPositiveIntException as er:
+        # print(er)
+        if er.code_error == ERROR_NON_INTEGER:
+            print("Задано значення не цілого типу")
+        elif er.code_error == ERROR_NON_POSITIVE_INTEGER:
+            print("Задане значення хоч і цілого типу, проте відʼємне")
